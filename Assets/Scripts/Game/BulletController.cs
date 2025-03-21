de7501fd71c77e19c45ea7a5e9e2e4e3544b6ff3
Kerich537 <unitykirillndt@gmail.com>
2025-03-21 17:48:40 +0300

    Fixed bullet damage

--- Assets/Scripts/Game/BulletController.cs
+++ Assets/Scripts/Game/BulletController.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class BulletController : MonoBehaviour
 {
     [SerializeField] private float _speed = 8.0f;
     private Rigidbody _rigidbody;
+    private bool _isLaunched = false;
 
     private void Start()
     {
@@ -23,9 +25,15 @@ public class BulletController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out IDamageble damageble))
+        if (other.TryGetComponent(out IDamageble damageble) && _isLaunched)
         {
             damageble.TakeDamage(100);
+            PhotonNetwork.Destroy(gameObject);
+        }
+
+        if (_isLaunched == false)
+        {
+            _isLaunched = true;
         }
     }
 }
